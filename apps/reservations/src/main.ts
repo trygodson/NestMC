31abcc6f7dfe5c508c80d7c980d7d2ce477af4ab
Godson <gnqnpn@gmail.com>
2024-06-09 01:37:42 +0100

    some changes rabbitmq

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -1,7 +1,15 @@
 // import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
 import { AbstractEntity } from './abstract.entity';
 import { Logger, NotFoundException } from '@nestjs/common';
-import { EntityManager, FindOptionsRelations, FindOptionsWhere, Repository } from 'typeorm';
+import {
+  DeepPartial,
+  EntityManager,
+  FindManyOptions,
+  FindOneOptions,
+  FindOptionsRelations,
+  FindOptionsWhere,
+  Repository,
+} from 'typeorm';
 import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 
 export abstract class AbstractRepository<T extends AbstractEntity<T>> {
@@ -22,8 +30,46 @@ export abstract class AbstractRepository<T extends AbstractEntity<T>> {
   //   return (await createdDocument.save()).toJSON() as unknown as TDocument;
   // }
 
-  async create(entity: T): Promise<T> {
-    return this.entityManager.save(entity);
+  public async save(data: DeepPartial<T>): Promise<T> {
+    return await this.entityRepository.save(data);
+  }
+  public async saveMany(data: DeepPartial<T>[]): Promise<T[]> {
+    return await this.entityRepository.save(data);
+  }
+  public create(data: T): Promise<T> {
+    return this.entityManager.save(data);
+  }
+  // public create(data: DeepPartial<T>): T {
+  //   return this.entityRepository.create(data);
+  // }
+  public createMany(data: DeepPartial<T>[]): T[] {
+    return this.entityRepository.create(data);
+  }
+
+  public async findOneById(id: any): Promise<T> {
+    const options: FindOptionsWhere<T> = {
+      id: id,
+    };
+    return await this.entityRepository.findOneBy(options);
+  }
+
+  public async findByCondition(filterCondition: FindOneOptions<T>): Promise<T> {
+    return await this.entityRepository.findOne(filterCondition);
+  }
+
+  public async findWithRelations(relations: FindManyOptions<T>): Promise<T[]> {
+    return await this.entityRepository.find(relations);
+  }
+
+  public async findAll(filterCondition?: FindManyOptions<T>): Promise<T[]> {
+    return await this.entityRepository.find(filterCondition);
+  }
+
+  public async remove(data: T): Promise<T> {
+    return await this.entityRepository.remove(data);
+  }
+  public async preload(entityLike: DeepPartial<T>): Promise<T> {
+    return await this.entityRepository.preload(entityLike);
   }
 
   async findOne(where: FindOptionsWhere<T>, relations?: FindOptionsRelations<T>): Promise<T> {
@@ -38,19 +84,6 @@ export abstract class AbstractRepository<T extends AbstractEntity<T>> {
     }
     return entity;
   }
-
-  // For mongoose
-
-  // async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
-  //   const document = await this.model.findOne(filterQuery, {}, { lean: true });
-
-  //   if (!document) {
-  //     this.logger.warn('Document Not Found', filterQuery);
-  //     throw new NotFoundException('Document Not Found');
-  //   }
-  //   return document as unknown as TDocument;
-  // }
-
   async findOneAndUpdate(
     where: FindOptionsWhere<T>,
     partialEntity: QueryDeepPartialEntity<T>,
